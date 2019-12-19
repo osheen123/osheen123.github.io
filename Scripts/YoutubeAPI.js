1f5227868f30f7b7185211f6ae113d99934aedb2
osheen123 <osheen.111@gmail.com>
2019-12-19 15:15:24 +0530

    Add files via upload

--- Scripts/YoutubeAPI.js
+++ Scripts/YoutubeAPI.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
 });
 
 $(window).scroll(function() {  
-  if($(window).scrollTop() + $(window).height() >= $(document).height()-250){     
+  if($(window).scrollTop() + $(window).height() >= $(document).height()-50){     
       if(!scrolltokenarray.includes(pageToken) && pageToken!=undefined) {
         //alert(pageToken)
         YoutubeAPI()
@@ -35,7 +35,7 @@ function YoutubeAPI(){
         AddVideoToParent(videoTitle,videoID,videoThumbnail)      
         if(pageToken==undefined)
         {          
-          $("#loader1").hide();
+          $("#loader").hide();
         }                  
       }catch(e){}      
     }
