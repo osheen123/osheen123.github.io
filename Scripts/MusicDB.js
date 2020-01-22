2f56ad4640652132b3e3d36aa917f58f54b53a1a
osheen123 <osheen.111@gmail.com>
2020-01-22 13:58:18 +0530

    Add files via upload

--- Scripts/MusicDB.js
+++ Scripts/MusicDB.js
@@ -1,6 +1,6 @@
 var musicdb=[
 
-    { //0
+    { //1
         "name" : "OSHEEN - COLOURS",
         "spotify" : "https://open.spotify.com/album/1Gkcl51qySRHpXePi2pdux",
         "google" : "https://play.google.com/store/music/album/Osheen_Colours?id=Bifvarln7wgamcp2zlyjejusj6m",
@@ -10,7 +10,7 @@ var musicdb=[
         "amazon" : "http://www.amazon.com/gp/product/B07CFKW283/?tag=distrokid06-20"        
     },
 
-    { //1
+    { //2
         "name" : "OSHEEN - BLISS",
         "spotify" : "https://open.spotify.com/album/3X5NLbPEgiJ92q61e1sK0w",
         "google" : "https://play.google.com/store/music/album/Osheen_Bliss?id=Btw7ztcwmx6ppx7jdiketz42lru",
@@ -20,7 +20,7 @@ var musicdb=[
         "amazon" : "http://www.amazon.com/gp/product/B07C3CT79V/?tag=distrokid06-20"        
     },
 
-    { //2
+    { //3
         "name" : "OSHEEN - AMBER",
         "spotify" : "https://open.spotify.com/album/7HpelUNOVSn24l1IMJFT7K",
         "google" : "https://play.google.com/store/music/album/Osheen_Amber?id=Bot4hyf2xh4uvq6ax5ujqsze7by",
@@ -30,16 +30,6 @@ var musicdb=[
         "amazon" : "http://www.amazon.com/gp/product/B07C33NCN7/?tag=distrokid06-20"        
     },
 
-    { //3
-        "name" : "OSHEEN - EYE ON YOU",
-        "spotify" : "https://open.spotify.com/album/3owJJ9zUdL71DvdWfWTChC",
-        "google" : "https://play.google.com/store/music/album/Osheen_Eye_on_You_feat_Nina_Sung?id=Baoivdptrbvq7pu3zdp667i3zpq",
-        "youtube" : "https://www.youtube.com/watch?v=zcG8_U789B4",
-        "deezer" : "https://www.deezer.com/album/65750152",
-        "apple" : "https://itunes.apple.com/us/album/eye-on-you-feat-nina-sung-single/1398594532?uo=4&app=itunes&at=1001lry3&ct=dashboard",
-        "amazon" : "https://open.spotify.com/album/3owJJ9zUdL71DvdWfWTChC"        
-    },
-
     { //4
         "name" : "OSHEEN - EYE ON YOU",
         "spotify" : "https://open.spotify.com/album/3owJJ9zUdL71DvdWfWTChC",
@@ -47,8 +37,99 @@ var musicdb=[
         "youtube" : "https://www.youtube.com/watch?v=zcG8_U789B4",
         "deezer" : "https://www.deezer.com/album/65750152",
         "apple" : "https://itunes.apple.com/us/album/eye-on-you-feat-nina-sung-single/1398594532?uo=4&app=itunes&at=1001lry3&ct=dashboard",
-        "amazon" : "https://open.spotify.com/album/3owJJ9zUdL71DvdWfWTChC"        
+        "amazon" : "https://www.amazon.com/Eye-You-feat-Nina-Sung/dp/B07DQ8B6DZ/"        
+    },
+
+    { //5
+        "name" : "OSHEEN - SOMERSAULT",
+        "spotify" : "https://open.spotify.com/album/3yfGlOdhWpWxqae1RrbmFZ",
+        "google" : "https://play.google.com/store/music/album/Osheen_Somersault?id=Bn2hscayn5mmplujc4bgnw3vtx4",
+        "youtube" : "https://www.youtube.com/watch?v=fviD9rr9Iqg",
+        "deezer" : "https://www.deezer.com/album/70000482",
+        "apple" : "https://itunes.apple.com/us/album/somersault-single/1422479365?uo=4&app=itunes&at=1001lry3&ct=dashboard",
+        "amazon" : "http://www.amazon.com/gp/product/B07G6LTDVL/?tag=distrokid06-20"        
+    },
+
+    { //6
+        "name" : "OSHEEN - ZIPPY",
+        "spotify" : "https://open.spotify.com/album/1HxzA0fNmqqCUB5F1ZvDIl",
+        "google" : "https://play.google.com/store/music/album/Osheen_Zippy?id=Bicx6tzqg6qbjrf2mpsknnh5qsu",
+        "youtube" : "https://www.youtube.com/watch?v=7IGaG_UYulY",
+        "deezer" : "https://www.deezer.com/album/70903412",
+        "apple" : "https://itunes.apple.com/us/album/zippy-single/1429050276?uo=4&app=music&at=1001lry3&ct=dashboard",
+        "amazon" : "http://www.amazon.com/gp/product/B07GK6M7MZ/?tag=distrokid06-20"
+    },
+
+    { //7
+        "name" : "OSHEEN - JOVIAL",
+        "spotify" : "https://open.spotify.com/album/5B2MSJ6GePCMEbRjLsWZMA",
+        "google" : "https://play.google.com/store/music/album/Osheen_Jovial?id=Bqzvj7t6qivvhfkaiajlhahlm7y",
+        "youtube" : "https://www.youtube.com/watch?v=Y0A82ShU2M0",
+        "deezer" : "https://www.deezer.com/album/80651482",
+        "apple" : "https://itunes.apple.com/us/album/jovial-single/1445313129?uo=4&app=music&at=1001lry3&ct=dashboard",
+        "amazon" : "http://www.amazon.com/gp/product/B07L22VDZ1/?tag=distrokid06-20"
+    },
+
+    { //8
+        "name" : "OSHEEN - LOOKING FOR YOU",
+        "spotify" : "https://open.spotify.com/album/5B2MSJ6GePCMEbRjLsWZMA",
+        "google" : "https://play.google.com/store/music/album/Osheen_Jovial?id=Bqzvj7t6qivvhfkaiajlhahlm7y",
+        "youtube" : "https://www.youtube.com/watch?v=Y0A82ShU2M0",
+        "deezer" : "https://www.deezer.com/album/80651482",
+        "apple" : "https://itunes.apple.com/us/album/jovial-single/1445313129?uo=4&app=music&at=1001lry3&ct=dashboard",
+        "amazon" : "http://www.amazon.com/gp/product/B07L22VDZ1/?tag=distrokid06-20"
+    },
+
+    { //9
+        "name" : "OSHEEN - GENIAL",
+        "spotify" : "https://open.spotify.com/album/5Go5WRyxWPTPP5rvrR1gQS",
+        "google" : "https://play.google.com/store/music/album/Osheen_Genial?id=B47jsgt2rungimrme322u376us4",
+        "youtube" : "https://www.youtube.com/watch?v=XPPralkZu0E",
+        "deezer" : "https://www.deezer.com/album/85920812",
+        "apple" : "https://itunes.apple.com/us/album/genial-single/1451252349?uo=4&app=music&at=1001lry3&ct=dashboard",
+        "amazon" : "http://www.amazon.com/gp/product/B07N87HN6H/?tag=distrokid06-20"
+    },
+
+    { //10
+        "name" : "OSHEEN - YOUNG",
+        "spotify" : "https://open.spotify.com/album/1Txu79nfFijMXq84CNIp25",
+        "google" : "https://play.google.com/store/music/album/Osheen_Young?id=Bcki34djkw6tf5z6o7pdaxjfvui",
+        "youtube" : "https://www.youtube.com/watch?v=kkW8TvQQQDA",
+        "deezer" : "https://www.deezer.com/album/86980632",
+        "apple" : "https://itunes.apple.com/us/album/young-single/1452778364?uo=4&app=music&at=1001lry3&ct=dashboard",
+        "amazon" : "http://www.amazon.com/gp/product/B07NKB4Q2R/?tag=distrokid06-20"
+    },
+
+    { //11
+        "name" : "OSHEEN - OUT LOUD",
+        "spotify" : "https://open.spotify.com/album/7FitMdcKrkJkYtQfY6IO2z",
+        "google" : "https://play.google.com/store/music/album?id=Btspqoipffuafqwdax5rvwuopoi&tid=song-Tcswj44w5eyzwpf7rrc36fbgbfm",
+        "youtube" : "https://www.youtube.com/watch?v=QLmI1n-E37A",
+        "deezer" : "https://www.deezer.com/album/89959672",
+        "apple" : "https://itunes.apple.com/us/album/out-loud-feat-shikhar-single/1455786418?uo=4&app=music&at=1001lry3&ct=dashboard",
+        "amazon" : "http://www.amazon.com/gp/product/B07PKLF648/?tag=distrokid06-20"
+    },
+
+    { //12
+        "name" : "OSHEEN - HAPPY ENDINGS",
+        "spotify" : "https://open.spotify.com/album/0fQtjEb2Xg5jFSzwZLOG61",
+        "google" : "https://play.google.com/store/music/album?id=Bdvyxchcbmj4bynqufscs5srybu&tid=song-Tamgat3mmqyffjzonhmdbiql5wu",
+        "youtube" : "https://www.youtube.com/watch?v=WPwMmsizXEg",
+        "deezer" : "https://www.deezer.com/album/90622122",
+        "apple" : "https://itunes.apple.com/us/album/happy-endings-single/1456682487?uo=4&app=itunes&at=1001lry3&ct=dashboard",
+        "amazon" : "http://www.amazon.com/gp/product/B07PR7KTQT/?tag=distrokid06-20"
+    },
+
+    { //13
+        "name" : "OSHEEN - BREAK",
+        "spotify" : "https://open.spotify.com/album/2Ny0xkZ4t93b26CaZAGVBf",
+        "google" : "https://play.google.com/store/music/album/Osheen_Break?id=Bfmwdmj4dkmwcmlfaolytiyscdi",
+        "youtube" : "https://www.youtube.com/watch?v=PM4Zbrh4-RM",
+        "deezer" : "https://www.deezer.com/album/119444772",
+        "apple" : "https://music.apple.com/us/album/break-single/1488260648?uo=4&app=music&at=1001lry3&ct=dashboard",
+        "amazon" : "https://music.amazon.in/albums/B081M9G6CD"
     }
+
 
     
 ];
@@ -68,7 +149,7 @@ $(document).ready(function(){
 
 function AddVideoCards(name,spotify,google,youtube,deezer,apple,amazon){
 
-    for (var i in musicdb)
+    for (var i in musicdb.reverse())
     {
         name = musicdb[i]["name"]
         spotify = musicdb[i]["spotify"]
